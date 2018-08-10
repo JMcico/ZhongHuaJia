b59c48d144ead10181f4cf6a0c7c4e9c672f60ed
JMcico <36693816+JMcico@users.noreply.github.com>
2018-08-10 22:38:02 +0800

    Initial commit

---
+++ app/src/main/java/com/example/zhonghuajia/myEyes.java
@@ -0,0 +1,13 @@
+package com.example.zhonghuajia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class myEyes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_eyes);
+    }
+}
